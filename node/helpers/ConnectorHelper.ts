16a0b706bf28ecd9abed73b64ef2fe43ad33ff6d
Radu Gabor <radu.gabor@hotmail.com>
2022-04-21 18:54:09 +0300

    Log emag errors; Stop product notify if different affiliate id;

--- node/helpers/ConnectorHelper.ts
+++ node/helpers/ConnectorHelper.ts
@@ -4,7 +4,7 @@ export function getAppSettings(ctx: IOContext): Promise<AppSettings> {
   return new Promise((resolve, reject) => {
     const apps = new Apps(ctx);
     return apps
-      .getAppSettings("zitec.emag-connector")
+      .getAppSettings("vtex.emag-connector")
       .then((response) => {
         resolve(response);
       })
